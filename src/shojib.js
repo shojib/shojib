06623d612b7e662638f648cf81cacdd1bd5d37d4
mhossain <mahmud.hossain@mlb.com>
2016-02-18 12:22:44 -0500

    first working code

---
+++ src/shojib.js
@@ -0,0 +1,134 @@
+var TEMPLATE_REGEX = {
+    curlyRe: /{{([^}}]+)}}/g,
+    tagRe: /<(.*?)>/g,
+    endRe: /(<\/[a-z]+?>)/g,
+    forRe: /for="(.*?)"/g,
+    ifRe: /if="(.*?)"/g,
+    varRe: /[^{}]/g,
+    stripCurly: /[a-z.A-Z0-9_]+/g,
+    stripFor: /for="(.*[^"])/g
+}
+
+function isMapFn (obj) {
+    if (typeof obj === 'object' || Array.isArray(obj)) {
+        var str = JSON.stringify(obj);   
+        var match = str.match(/[{}]/g)
+        return match ? match.length > 2 : false
+    } else {
+        return false
+    }
+}
+
+function docFragmentFn (html) {
+    var frag = document.createDocumentFragment(),
+    tmp = document.createElement('body'), child;
+    tmp.innerHTML = html;
+    while (child = tmp.firstElementChild) {
+        frag.appendChild(child);
+    }
+    return frag;
+}
+
+function stringify (obj) {
+    return JSON.stringify(obj)
+}
+
+function isDefined (obj) {
+    return typeof obj !== 'undefined'
+}
+
+function isNull (obj) {
+    return obj === null
+}
+
+function removeCurly (str) {
+    if (isDefined(str))
+        return str.match(TEMPLATE_REGEX.stripCurly).pop()
+}
+
+function removeFor (str) {
+    var re = str.match(TEMPLATE_REGEX.stripFor)
+    return RegExp.$1
+}
+
+function rmCurlySplit (match) {
+    if (isDefined(match))
+        return removeCurly(match).split('.')[1]
+}
+
+var Curly = function (match, fragment, model) {
+    var frag, regexp = new RegExp(match[0], 'g')
+    if (isDefined(model[match[1]])) {
+        frag = fragment.replace(regexp, model[match[1]])
+        return frag
+    } else {
+        return fragment
+    }
+}
+
+var ForLoop = function (match, model) {
+    var match, syntax, valName, iterable, compiledTpl = '',
+    arrays = match[1].split(' ')
+
+    if (arrays.length === 3) {
+        objKey = arrays[0]
+        separator = arrays[1] === 'in'
+        
+        var scopes = Object.keys(model), len = scopes.length
+        for (var j = 0; j < len; j++) {
+            if (arrays[2] === scopes[j]) {
+                iterable = scopes[j]
+                break
+            }
+        }
+        
+        if (separator && isDefined(iterable)) {
+            var data = model[iterable],
+                keys = Object.keys(data),
+                len = keys.length,
+                fMatch, frag
+            for (var i = 0; i < len; i++) {
+                frag = match.input
+                while (fMatch = TEMPLATE_REGEX.curlyRe.exec(match.input)) {
+                    fMatch[1] = fMatch[1].split('.')[1]
+                    frag = Curly(fMatch, frag, data[i])
+                }
+                compiledTpl += frag
+                console.log(i, compiledTpl)
+            }
+        }
+    }
+    return compiledTpl
+}
+
+var View = function (opts) {
+    var opts = opts || {},
+        template = opts.template.trim(),
+        model = opts.model,
+        scopes = Object.keys(model),
+        element = opts.element,
+        match, year = new Date().getFullYear(),
+        frag = docFragmentFn(template),
+        fragments = frag.querySelectorAll('*'),
+        fragmentsLength = fragments.length
+        model.year = year
+
+    for (var i = 0; i < fragmentsLength; i++) {
+        var frag = fragments[i].childNodes[0].data
+        while (match = TEMPLATE_REGEX.curlyRe.exec(fragments[i].innerHTML)) {
+            frag = Curly(match, frag, model)
+        }
+        fragments[i].replaceChild(document.createTextNode(frag), fragments[i].childNodes[0])
+        
+        while (match = TEMPLATE_REGEX.forRe.exec(fragments[i].innerHTML)) {
+            frag = fragments[i].innerHTML
+            frag = ForLoop(match, model)
+        }
+        fragments[i].replaceChild(document.createTextNode(frag), fragments[i].childNodes[0])
+    }
+
+    console.log(fragments)
+    var ele = document.querySelector(element).appendChild(fragments[1])
+
+}
+
